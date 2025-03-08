85c0dddb32c1c5321157eaf69ea174c45dfa2bd4
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2025-03-08 20:19:23 +0000

    Env project

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+
+class Semaforo {
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int contador;
+
+public:
+    Semaforo(int valor) : contador(valor) {}
+
+    void acquire() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]() { return contador > 0; });
+        contador--;
+    }
+
+    void release() {
+        std::unique_lock<std::mutex> lock(mtx);
+        contador++;
+        cv.notify_one();
+    }
+};
+
+Semaforo semaforo(2); // Permite até 2 threads simultâneas
+//using namespace std;
+
+int main(){
+
+    return 0;
+}
