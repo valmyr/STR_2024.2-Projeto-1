c966e43c859dc88ae6650e42999f38d59560bce5
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2025-03-08 21:59:24 +0000

    Removendo algumas linhas

--- main.cpp
+++ main.cpp
@@ -4,28 +4,6 @@
 #include<queue>
 #include <unistd.h> // For sleep()
 using namespace std;
-sem_t semaphore; // Semaphore
-int count = 0;
-
-// void* producer(void* arg) {
-//     while (true) {
-//         sleep(1); // Simulate work
-//         count++;
-//         std::cout << "Producer: Producing item. It has "<< count << " itens\n";
-//         sem_post(&semaphore); // Release (increment) semaphore
-//     }
-//     return nullptr;
-// }
-
-// void* consumer(void* arg) {
-//     while (true) {
-//         sem_wait(&semaphore); // Wait (decrement) semaphore
-//         count--;
-//         std::cout << "Consumer: Consuming item. It has "<< count << " itens\n";
-//         sleep(1); // Simulate work
-//     }
-//     return nullptr;
-// }
 struct Usuario{
     int andar_atual;
     int andar_alvo;
@@ -52,21 +30,7 @@ void Elevador::solicitacao(int andarAtual, int andarDestino){
 }
 
 int main() {
-    pthread_t RequestThread, Move_solThread, Move_deliveThread;
 
-    // Initialize semaphore with value 0
-    sem_init(&semaphore, 0, 0);
-
-    // Create threads
-    pthread_create(&producerThread, nullptr, producer, nullptr);
-    pthread_create(&consumerThread, nullptr, consumer, nullptr);
-
-    // Join threads (infinite loop, will run forever)
-    pthread_join(producerThread, nullptr);
-    pthread_join(consumerThread, nullptr);
-
-    // Destroy semaphore (not reached in this example)
-    sem_destroy(&semaphore);
 
     return 0;
 }
