0a322cd43dbfdecb29c65651a0f8ba34201f71ed
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2025-03-08 21:57:57 +0000

    Inicializando projeto

--- main.cpp
+++ main.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
+#include<queue>
 #include <unistd.h> // For sleep()
-
+using namespace std;
 sem_t semaphore; // Semaphore
 int count = 0;
 
-void* producer(void* arg) {
-    while (true) {
-        sleep(1); // Simulate work
-        count++;
-        std::cout << "Producer: Producing item. It has "<< count << " itens\n";
-        sem_post(&semaphore); // Release (increment) semaphore
-    }
-    return nullptr;
-}
+// void* producer(void* arg) {
+//     while (true) {
+//         sleep(1); // Simulate work
+//         count++;
+//         std::cout << "Producer: Producing item. It has "<< count << " itens\n";
+//         sem_post(&semaphore); // Release (increment) semaphore
+//     }
+//     return nullptr;
+// }
 
-void* consumer(void* arg) {
-    while (true) {
-        sem_wait(&semaphore); // Wait (decrement) semaphore
-        count--;
-        std::cout << "Consumer: Consuming item. It has "<< count << " itens\n";
-        sleep(1); // Simulate work
-    }
-    return nullptr;
+// void* consumer(void* arg) {
+//     while (true) {
+//         sem_wait(&semaphore); // Wait (decrement) semaphore
+//         count--;
+//         std::cout << "Consumer: Consuming item. It has "<< count << " itens\n";
+//         sleep(1); // Simulate work
+//     }
+//     return nullptr;
+// }
+struct Usuario{
+    int andar_atual;
+    int andar_alvo;
+};
+
+class Elevador{
+    public:
+        int andar_corrente;
+        int idUser;
+        void solicitacao(int,int);
+    private:
+        pthread_t Requisicao;
+        void mover_para_solictacao();
+        void mover_para_destino();
+    Elevador();
+    ~Elevador();
+};
+Elevador::Elevador(){andar_corrente = 0; idUser = 0;}
+Elevador::~Elevador(){}
+void Elevador::solicitacao(int andarAtual, int andarDestino){
+    idUser+=1;
+    cout<<"[Usuario "<< idUser <<" ] chamando o elevador do andar"<< andar_corrente << "para o andar"<<andarDestino<<endl;
+
 }
 
 int main() {
-    pthread_t producerThread, consumerThread;
+    pthread_t RequestThread, Move_solThread, Move_deliveThread;
 
     // Initialize semaphore with value 0
     sem_init(&semaphore, 0, 0);
