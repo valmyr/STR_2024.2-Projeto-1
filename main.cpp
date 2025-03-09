ee54fe1ae826f1fa427ebcdc6336d6a0bef2838d
AndrewPOliveira <jose.pessoa@ee.ufcg.edu.br>
2025-03-08 23:53:40 -0300

    Thread Elevador
    Busca user sem semáforo

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int andarDestino;
 static int andarCorrente = 0;
 static int idUser = 0;
 static void *solicitacao(void *arg);
+static void *buscaUsuario(void *arg);
 void mover_para_solictacao();
 void mover_para_destino();
 
@@ -22,9 +23,11 @@ void mover_para_destino();
 
 int main() {
     srand(time(NULL));
-    pthread_t soliciatcaoThread;
+    pthread_t soliciatcaoThread, elevadorThread;
     pthread_create(&soliciatcaoThread, nullptr,solicitacao, nullptr);
+    pthread_create(&elevadorThread, nullptr, buscaUsuario, nullptr);
     pthread_join(soliciatcaoThread, nullptr);
+    pthread_join(elevadorThread, nullptr);
     return 0;
 }
 static void *solicitacao(void *arg){
@@ -35,12 +38,47 @@ static void *solicitacao(void *arg){
         do{
             andarAtual =rand()%6;
         }while(andarDestino == andarAtual);
-        cout<<"[    Usuario "<< idUser <<"  ] Chamando o elevador do andar  "<< andarCorrente << "  para o andar    "<<andarDestino<<endl;
-        sleep(rand()%4);
+        cout<<"[Usuario "<< idUser <<"] Chamando o elevador do andar  "<< andarAtual << "  para o andar    "<<andarDestino<<endl;
+
+        sleep(rand()%4+4);
         user.andarAtual = andarAtual;
         user.andarDestino = andarDestino;
         user.idUser = idUser;
+        
         fila.push(user);
+        
+        cout<< fila.size()<<endl;
+    }
+    return nullptr;
+}
+
+static void *buscaUsuario(void *arg){
+    Usuario user;
+    while(true){
+        if(fila.size() > 0)
+        {
+            user = fila.front();
+
+        cout<<endl<<"[Elevador]" <<" Nova chamada recebida de Usuário  "<< user.idUser << ":  Andar  "<<user.andarAtual<<" -> "<<user.andarDestino<<"."<<endl;
+        sleep(1);
+        cout<<"[Elevador] Movendo-se para o andar "<<user.andarAtual<<"..."<<endl;
+        andarCorrente = user.andarAtual;
+        sleep(1);
+        cout<<"[Elevador] Chegou ao andar "<<andarCorrente<<". Usuário "<<user.idUser<<" embarcou."<<endl;
+        sleep(1);
+        int distanciaRelativa = user.andarAtual-user.andarDestino > 0 ? user.andarAtual-user.andarDestino:-(user.andarAtual-user.andarDestino);
+
+        for(int i=0; i < distanciaRelativa; i++){
+            (andarCorrente > user.andarDestino) ?  andarCorrente-- :  andarCorrente++;
+            cout<<"[Elevador] Movendo-se para o andar "<<andarCorrente<<"..."<<endl;
+            sleep(1);
+        }
+
+        cout<<"[Elevador] Chegou ao andar "<<andarCorrente<<". Usuário "<<user.idUser<<" desembarcou."<<endl;
+        sleep(1);
+        fila.pop();
+        }
+        
     }
     return nullptr;
 }
